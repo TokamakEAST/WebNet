358d8bd5af451e42c935ffe0f48094b788aca822
Together_Liu <l162193@icloud.com>
2024-06-18 23:55:37 +0800

    还得是我，修高了

--- getqntable.php
+++ getqntable.php
@@ -1,35 +1,39 @@
 <?php
  
-$con = mysqli_connect('localhost', 'wk', 'TkJb6kCTLGCbRyTM'); // 连接数据库服务器
-if (!$con) {
+$con = mysqli_connect('localhost','wk','TkJb6kCTLGCbRyTM');//链接数据库服务器
+if (!$con)
+{
     die('Could not connect: ' . mysqli_error($con));
 }
+// 选择数据库
+mysqli_select_db($con,"wk");                           //数据库名
+// 设置编码，防止中文乱码
+mysqli_set_charset($con, "utf8");
+ 
+$sql="SELECT time,speed,temp,dire,hum FROM wkqn ORDER BY time desc LIMIT 15";                      //数据库下属表名
 
-mysqli_select_db($con, "wk"); // 选择数据库
-mysqli_set_charset($con, "utf8"); // 设置编码，防止中文乱码
+$result = mysqli_query($con,$sql);
 
-// 编写SQL语句，选择特定的15个列和时间列
-$sql = "SELECT time, speed, temp, dire, hum FROM wkqn ORDER BY time DESC LIMIT 10";
-
-$result = mysqli_query($con, $sql);
-
-if (!$result) {
-    printf("Error: %s\n", mysqli_error($con));
-    exit();
+if (!$result) 
+{
+printf("Error: %s\n", mysqli_error($con));
+exit();
 }
 
-$timedata = [];
-$columnsData = array_fill(1, 15, []); // 创建一个包含15个空数组的数组，对应15个列
+$spedata = [];
+$tempdata = [];
+$diredata = [];
+$humdata = [];
 
-while ($row = mysqli_fetch_array($result)) {
-    $timedata[] = $row['time'];
-    for ($i = 1; $i <= 15; $i++) {
-        $columnsData[$i][] = $row["column$i"];
-    }
+while($row = mysqli_fetch_array($result))
+{
+    $spedata[] = $row['speed'];
+    $tempdata[] = $row['temp'];
+    $diredata[] = $row['dire'];
+    $humdata[] = $row['hum'];
 }
-
-$jsondata = array_merge([$timedata], $columnsData); // 合并时间数据和其他列数据
-echo json_encode($jsondata);
+    $jsondata = array($spedata,$tempdata,$diredata,$humdata);
+    echo json_encode($jsondata);
 
 mysqli_close($con);
 
